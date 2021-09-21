0989da241c16ccacc051931c8442257f6f8c892e
Nnamdi Okeke <nahorr@gmail.com>
2021-09-21 12:40:27 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Contact Us Package
+
+## This will send email to admin and save contact query in db
---
+++ src/Http/Controllers/ContactController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Nnamdi\Contact\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Nnamdi\Contact\Mail\ContactMailable;
+use Nnamdi\Contact\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contact::contact');
+    }
+
+    public function send(Request $request)
+    {
+
+        Mail::to(config('contact.send_email_to'))->send(new ContactMailable($request->message, $request->name));
+
+        Contact::create($request->all());
+
+        return back();
+    }
+}
